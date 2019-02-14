f0361ea8db14a4eeb908a74553dca2e3cbf7ae6e
Matthew Schultz <mschult7@binghamton.edu>
2019-02-14 17:45:31 -0500

    list updated

---
+++ List.cpp
@@ -0,0 +1,82 @@
+#include "List.h"
+
+//Constructor
+List::List(){
+	this->head = NULL;
+	this->tail = NULL;
+	size = 0;
+}
+
+//Destructor
+List::~List(){
+	Node * curr = this.head();
+	
+	for(int i=0;i<size;i++){
+		Node * n = curr->next;
+		delete curr;
+		curr = n;
+	}
+	this->head = NULL;
+	this->tail = NULL; 
+}
+
+//get head() and tail()
+Node * List::head(){
+	return this->head;
+}
+Node * List::tail(){
+	return this->tail;
+}
+
+//inserts an element at index, increasing the List size by 1
+//if the insert index is out of bounds, you should append to the end of the list
+void List::insert(int index, Planet * p){
+	Node curr = new Node();
+	curr->planetData = p;
+	if(this.head()==NULL){
+		this->head = curr;
+		this->tail = curr;
+		curr->next = NULL;
+		curr->prev = NULL; 
+	} else if(index > size){
+		this.tail()->next = curr;
+		curr->prev = this.tail();
+		this->tail = curr;
+		curr->next = NULL;
+		
+	} else{
+		Node * n = this.head();
+		for(int i=0;i<index-1;i++){
+			n = n->next;
+		}
+		curr->prev = n;
+		curr->next = n->next;
+		n->next->prev = curr;
+	}
+	this->size++;
+}
+
+Planet * List::read(int index){
+	if(index>size){
+		return NULL;
+	} else{
+		Planet * retPlanet;
+		Node * curr = this.head();
+		for(int i=0;i<index;i++){
+			curr = curr->next;
+		}
+	}
+	return curr->dataPlanet;
+	
+}
+
+bool List::remove(int index){
+	Node * n = this.head();
+	for(int i = 0; i<index;i++){
+		n = n->next;
+	}
+	n->prev->next = n->next;
+	n->next->prev = n->prev;
+	delete n;
+	this->size--;
+}
--- List.h
+++ List.h
@@ -1,20 +1,23 @@
 #ifndef LIST_H
 #define LIST_H
-
+#include <iostream>
 class Node{
    	private:
 		Node * next;
 		Node * prev;
-		int data;	
+		Planet * planetData;	
 }
 
 class List{
 	private:
-		Node * currentNode;
-		
+		Node * head;
+		Node * tail;
+		int size;
 	public:
 		List();
 		~List();
+		Node * head();
+		Node * tail();
 		void insert(int index, Planet * p);
 		Planet * read(int index);
 		bool remove(int index);
